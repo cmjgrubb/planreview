6cdf3464f004a357667ede201885f4839acc04dc
CMJ Grubb <cmj.grubb@icloud.com>
2024-01-31 15:14:11 -0500

    Working on formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,227 +1,330 @@
 <script lang="ts">
-  let project: string = "";
-  let applicant: string = "";
-  let attention: string = "";
-  let review_number: string = "";
-    let approval_status: string = "";
-    let today = new Date();
-    let deadline = new Date();
-    deadline.setDate(today.getDate() + 21);
-    let formatted_today = today.toLocaleDateString();
-    let formatted_deadline = deadline.toLocaleDateString();
-    let onsite_easement: Boolean | null;
-    let offsite_easement: Boolean | null;
-    let deq_approval: Boolean | null;
-    let vdh_approval: Boolean | null;
-    let vdh_report: Boolean | null;
-    let pump_station: Boolean | null;
+	let project: string = '';
+	let applicant: string = '';
+	let attention: string = '';
+	let review_number: string = '';
+	let approval_status: string = '';
+	let today = new Date();
+	let deadline = new Date();
+	deadline.setDate(today.getDate() + 21);
+	let formatted_today = today.toLocaleDateString();
+	let formatted_deadline = deadline.toLocaleDateString();
+	let onsite_easement: Boolean | null;
+	let offsite_easement: Boolean | null;
+	let deq_approval: Boolean | null;
+	let vdh_approval: Boolean | null;
+	let vdh_report: Boolean | null;
+	let pump_station: Boolean | null;
 
-    function handle_submit(event: Event) {
-      window.print();
+	function handle_submit(event: Event) {
+		let empty_fields = [];
+		if (!project) empty_fields.push('Project');
+		if (!applicant) empty_fields.push('Applicant');
+		if (!attention) empty_fields.push('Attention');
+		if (!review_number) empty_fields.push('Review Number');
+		if (!approval_status) empty_fields.push('Approval Status');
+		if (!formatted_deadline) empty_fields.push('Deadline');
+		if (onsite_easement === null) empty_fields.push('Onsite Easement');
+		if (offsite_easement === null) empty_fields.push('Offsite Easement');
+		if (deq_approval === null) empty_fields.push('DEQ Approval');
+		if (vdh_approval === null) empty_fields.push('VDH Approval');
+		if (vdh_report === null) empty_fields.push('VDH Report');
+		if (pump_station === null) empty_fields.push('Pump Station');
 
-      project = "";
-     applicant = "";
-     attention = "";
-     review_number = "";
-     approval_status = "";
-    today = new Date();
-    deadline = new Date();
-    deadline.setDate(deadline.getDate() + 21);
-    formatted_today = today.toLocaleDateString();
-    formatted_deadline = deadline.toLocaleDateString();
-    onsite_easement = null;
-    offsite_easement = null;
-    deq_approval = null;
-    vdh_approval = null;
-    vdh_report = null;
-    pump_station = null;
-    }
-  </script>
+		if (empty_fields.length > 0) {
+			alert(`Please fill out the following fields: ${empty_fields.join(', ')}`);
+			return;
+		}
+		window.print();
 
-<div class="contact_info">
-    <div class="left_flexbox">
-      <p>PO Box 1877, Winchester, VA 22604-8377</p>
-      <p>315 Tasker Road, Stephens City, VA 22655</p>
-    </div>
-    <div class="right_flexbox">
-      <p>540.868.1061 voice</p>
-      <p>540.868.1429 fax</p>
-    </div>
-  </div>
+		project = '';
+		applicant = '';
+		attention = '';
+		review_number = '';
+		approval_status = '';
+		today = new Date();
+		deadline = new Date();
+		deadline.setDate(deadline.getDate() + 21);
+		formatted_today = today.toLocaleDateString();
+		formatted_deadline = deadline.toLocaleDateString();
+		onsite_easement = null;
+		offsite_easement = null;
+		deq_approval = null;
+		vdh_approval = null;
+		vdh_report = null;
+		pump_station = null;
+	}
+</script>
 
-  <form on:submit|preventDefault={handle_submit}>
-    <div>
-    <label for="project">Project</label>
-    <input type="text" id="project" name="project" bind:value={project} placeholder="Project Name" required />
+<form on:submit|preventDefault={handle_submit}>
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			<label for="project">Project: </label>
+			<input
+				type="text"
+				id="project"
+				name="project"
+				bind:value={project}
+				placeholder="Project Name"
+			/>
+		</div>
 
-    <label for="Applicant">Applicant</label>
-    <input type="text" id="Applicant" name="Applicant" bind:value={applicant} placeholder="Applicant Name" required />
-</div>
+		<div class="inner_flexbox">
+			<label for="Applicant">Applicant: </label>
+			<input
+				type="text"
+				id="Applicant"
+				name="Applicant"
+				bind:value={applicant}
+				placeholder="Applicant Name"
+			/>
+		</div>
 
-    <label for="Attention">Attention</label>
-    <input type="text" id="Attention" name="Attention" bind:value={attention} placeholder="Attention" />
+		<div class="inner_flexbox">
+			<label for="Attention">Attention: </label>
+			<input
+				type="text"
+				id="Attention"
+				name="Attention"
+				bind:value={attention}
+				placeholder="Attention"
+			/>
+		</div>
+	</div>
 
-    <h2>Review Status</h2>
+	<h2>Review Status</h2>
 
-    <label for="Review_Number">Review Number</label>
-    <input type="text" id="Review_Number" name="Review_Number" bind:value={review_number} required />
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			<label for="Review_Number">Review Number: </label>
+			<input type="text" id="Review_Number" name="Review_Number" bind:value={review_number} />
+		</div>
 
-    <div>
-    <label>
-        <input type="radio" bind:group={approval_status} value="correct">
-        Correct & Resubmit
-      </label>
-    
-      <label>
-        <input type="radio" bind:group={approval_status} value="noted">
-        Approved as Noted
-      </label>
-    
-      <label>
-        <input type="radio" bind:group={approval_status} value="approved">
-        Approved
-      </label>
-    </div>
+		<div class="inner_flexbox">
+			<label>
+				<input type="radio" bind:group={approval_status} value="correct" />
+				Correct & Resubmit
+			</label>
 
-    <h2>Drawings</h2>
-    <label for="Deadline">Not later than</label>
-    <input type="text" id="Deadline" name="Deadline" placeholder={formatted_deadline} />
+			<label>
+				<input type="radio" bind:group={approval_status} value="noted" />
+				Approved as Noted
+			</label>
 
-    <ol>
-      <li>Hard copy of the entire approved plan set to the address above.</li>
-      <li>A digital copy of the entire approved plan set as a multi-page PDF to <a href="mailto:admin@frederickwater.com">admin@frederickwater.com</a>.</li>
-    </ol>
+			<label>
+				<input type="radio" bind:group={approval_status} value="approved" />
+				Approved
+			</label>
+		</div>
+	</div>
 
-    <h2>Easements</h2>
-    <div>
-      <label>
-      <input type="radio" bind:group={onsite_easement} value=true>
-    Y
-    </label>
-    <label>
-      <input type="radio" bind:group={onsite_easement} value=false>
-      N
-    </label>
-    <p>A copy of the recorded onsite Frederick Water (FW) easement(s) is/are required.</p>
-    </div>
+	<h2>Drawings</h2>
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			<label for="Deadline">Not later than</label>
+			<input type="text" id="Deadline" name="Deadline" placeholder={formatted_deadline} />
+		</div>
 
-    <div>
-      <label>
-        <input type="radio" bind:group={offsite_easement} value=true>
-        Y
-      </label>
-      <label>
-        <input type="radio" bind:group={offsite_easement} value=false>
-        N
-      </label>
-      <ul>
-        <li>A copy of the recorded offsite FW easement(s) is/are required.</li>
-        <li>The easement document shall contain a plat and both shall be signed by FW.</li>
-        <li>FW requires specific language in its deed of easement.</li>
-        <li>Visit our website (<a href="https://frederickwater.com">frederickwater.com</a>) to obtain a copy of the form.</li>
-        <li>A copy of the recorded deed(s) of easement is required before work is to begin on offsite property.</li>
-      </ul>
-    </div>
+		<div class="inner_flexbox">
+			<ol>
+				<li>Hard copy of the entire approved plan set to the address above.</li>
+				<li>
+					A digital copy of the entire approved plan set as a multi-page PDF to <a
+						href="mailto:admin@frederickwater.com">admin@frederickwater.com</a
+					>.
+				</li>
+			</ol>
+		</div>
+	</div>
 
-    <h2>Other</h2>
-    <div>
-      <label>
-      <input type="radio" bind:group={deq_approval} value=true>
-      Y
-    </label>
-    <label>
-      <input type="radio" bind:group={deq_approval} value=false>
-      N
-    </label>
-    <p>This project also requires DEQ &ndash; Valley Region Office approval.</p><br />
-    </div>
-    <div>
-    <label>
-      <input type="radio" bind:group={vdh_approval} value=true>
-      Y
-    </label>
-    <label>
-      <input type="radio" bind:group={vdh_approval} value=false>
-      N
-    </label>
-    <p>This project also requires VDH &ndash; Culpeper Field Office approval.</p><br />
-  </div>
-  <div>
-    <label>
-      <input type="radio" bind:group={vdh_report} value=true>
-      Y
-    </label>
-    <label>
-    <input type="radio" bind:group={vdh_report} value=false>
-      N
-    </label>
-    <p>This project requires VDH &ndash; Culpeper Field Office &ndash; Project Summary Report <i>by date for drawings above</i> (see website for form).</p><br />
-  </div>
-  <div>
-    <label>
-      <input type="radio" bind:group={pump_station} value=true>
-      Y
-    </label>
-    <label>
-      <input type="radio" bind:group={pump_station} value=false>
-      N
-    </label>
-    <p>This project has a sewer pump station that FW will own. Title to the pump station site shall be granted to FW in fee simple absolute. Also, (1) a copy of DEQ's Certificate to Construct, (2) a copy of DEQ's Certificate to Operate, and (3) two copies of the FW-approved O&M manual are required. These documents must be received before service will be authorized.</p>
-  </div>
-    <button type="submit">Print</button>
-  </form>
+	<h2>Easements</h2>
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			<label>
+				<input type="radio" bind:group={onsite_easement} value="true" />
+				Y
+			</label>
+			<label>
+				<input type="radio" bind:group={onsite_easement} value="false" />
+				N
+			</label>
+		</div>
+		<div class="inner_flexbox">
+			<p>A copy of the recorded onsite Frederick Water (FW) easement(s) is/are required.</p>
+		</div>
+	</div>
 
-  <p class="warning">Work cannot commence that would connect to FW facilities or require FW inspections until the contract, as applicable and sent to owner under separate cover, is returned to this office.</p><br />
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			<label>
+				<input type="radio" bind:group={offsite_easement} value="true" />
+				Y
+			</label>
+			<label>
+				<input type="radio" bind:group={offsite_easement} value="false" />
+				N
+			</label>
+		</div>
+		<div class="inner_flexbox">
+			<ul>
+				<li>A copy of the recorded offsite FW easement(s) is/are required.</li>
+				<li>The easement document shall contain a plat and both shall be signed by FW.</li>
+				<li>FW requires specific language in its deed of easement.</li>
+				<li>
+					Visit our website (<a href="https://frederickwater.com">frederickwater.com</a>) to obtain
+					a copy of the form.
+				</li>
+				<li>
+					A copy of the recorded deed(s) of easement is required before work is to begin on offsite
+					property.
+				</li>
+			</ul>
+		</div>
+	</div>
 
-  <p>Date: {formatted_today}</p>
-  <div>
-    <div class="left_flexbox">
-      <p>Earl W. Wiley &ndash; Engineering Assistant</p>
-    </div>
-    <div class="right_flexbox">
-      <p>AUG 23</p>
-    </div>
-  </div>
-  <style>
-    :global(body) {
-      font-family: Arial, sans-serif;
-      margin: 2rem;
-      padding: 1rem;
-      line-height: 2;
-    }
+	<h2>Other</h2>
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			<label>
+				<input type="radio" bind:group={deq_approval} value="true" />
+				Y
+			</label>
+			<label>
+				<input type="radio" bind:group={deq_approval} value="false" />
+				N
+			</label>
+		</div>
+		<div class="inner_flexbox">
+			<p>This project also requires DEQ &ndash; Valley Region Office approval.</p>
+		</div>
+	</div>
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			<label>
+				<input type="radio" bind:group={vdh_approval} value="true" />
+				Y
+			</label>
+			<label>
+				<input type="radio" bind:group={vdh_approval} value="false" />
+				N
+			</label>
+		</div>
+		<div class="inner_flexbox">
+			<p>This project also requires VDH &ndash; Culpeper Field Office approval.</p>
+		</div>
+	</div>
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			<label>
+				<input type="radio" bind:group={vdh_report} value="true" />
+				Y
+			</label>
+			<label>
+				<input type="radio" bind:group={vdh_report} value="false" />
+				N
+			</label>
+		</div>
+		<div class="inner_flexbox">
+			<p>
+				This project requires VDH &ndash; Culpeper Field Office &ndash; Project Summary Report<i>
+					by date for drawings above</i
+				> (see website for form).
+			</p>
+		</div>
+	</div>
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			<label>
+				<input type="radio" bind:group={pump_station} value="true" />
+				Y
+			</label>
+			<label>
+				<input type="radio" bind:group={pump_station} value="false" />
+				N
+			</label>
+		</div>
+		<div class="inner_flexbox">
+			<p>
+				This project has a sewer pump station that FW will own. Title to the pump station site shall
+				be granted to FW in fee simple absolute. Also, (1) a copy of DEQ's Certificate to Construct,
+				(2) a copy of DEQ's Certificate to Operate, and (3) two copies of the FW-approved O&M manual
+				are required. These documents must be received before service will be authorized.
+			</p>
+		</div>
+	</div>
 
-    button {
-      background-color: #014b92;
-      color: #fff;
-      padding: 0.5rem 1rem;
-      border: none;
-    }
+	<p class="warning">
+		Work cannot commence that would connect to FW facilities or require FW inspections until the
+		contract, as applicable and sent to owner under separate cover, is returned to this office.
+	</p>
 
-    .contact_info {
-      margin-bottom: 1rem;
-      font-size: 0.8rem;
-      line-height: 0.8;
-      display: flex;
-      justify-content: space-between;
-    }
+	<div class="outer_flexbox">
+		<div class="inner_flexbox">
+			Date: {formatted_today}
+		</div>
+		<div class="inner_flexbox">Earl W. Wiley &ndash; Engineering Assistant</div>
+		<div class="inner_flexbox">FEB 24</div>
+	</div>
+	<button type="submit">Print</button>
+</form>
 
-    .left_flexbox {
-      flex: 1;
-    }
+<style>
+	:global(body) {
+		font-family: Arial, sans-serif;
+		font-size: 12px;
+		margin: 2rem;
+		padding: 1rem;
+		line-height: 2;
+	}
 
-    .right_flexbox {
-      flex: 1;
-      text-align: right;
-    }
+	button {
+		background-color: #014b92;
+		color: #fff;
+		padding: 0.5rem 1rem;
+		border: none;
+		margin-top: 2rem;
+	}
 
-    .warning {
-      background-color: yellow;
-      padding: 1rem;
-    }
-    
-    @media print {
-  button {
-    display: none;
-  }
-}
-  </style>
+	h2 {
+		font-size: 14px;
+	}
+
+	ol {
+		margin-top: 0px;
+	}
+
+	ul {
+		margin-top: 0px;
+	}
+
+	.outer_flexbox {
+		display: flex;
+	}
+
+	.inner_flexbox {
+		margin-right: 2rem;
+		max-width: 80%;
+	}
+
+	.inner_flexbox p {
+		margin-top: 0px;
+	}
+
+	.warning {
+		background-color: yellow;
+		padding: 0.25rem 1rem;
+		margin-top: 0px;
+	}
+
+	@media print {
+		button {
+			display: none;
+		}
+
+		form {
+			width: 210mm;
+			height: 282mm;
+			font-size: 10px;
+		}
+	}
+</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,22 +1,62 @@
 <header>
-    <img src="/images/logo.png" alt="Logo">
-    <h1>Frederick Water Site Plan Review Comments</h1>
+	<div class="outer_flexbox">
+		<div class="left_flexbox">
+			<p>PO Box 1877, Winchester, VA 22604-8377</p>
+			<p>315 Tasker Road, Stephens City, VA 22655</p>
+		</div>
+		<div class="center_flexbox">
+			<img
+				src="/images/logo.png"
+				alt="Logo"
+				style="width: 200px; height: 50px; object-fit: contain"
+			/>
+			<h1>Site Plan Review Comments</h1>
+		</div>
+		<div class="right_flexbox">
+			<p>540.868.1061 voice</p>
+			<p>540.868.1429 fax</p>
+		</div>
+	</div>
+	<div class="break_line"></div>
 </header>
 
 <style>
-    header {
-        color: #014b92;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        margin-bottom: 2rem;
-        margin-top: 2rem;
-    }
+	header h1 {
+		font-family: Arial, sans-serif;
+		font-size: 16px;
+		margin-top: 1rem;
+	}
 
-    header h1 {
-        font-family: Arial, sans-serif;
-        font-size: 2rem;
-        margin-top: 1rem;
-    }
+	.outer_flexbox {
+		margin-bottom: 1rem;
+		font-size: 10px;
+		line-height: 0.5;
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-end;
+	}
+
+	.left_flexbox {
+		text-align: left;
+	}
+
+	.center_flexbox {
+		color: #014b92;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+	.right_flexbox {
+		text-align: right;
+	}
+
+	.break_line {
+		width: 90%;
+		border-bottom: 5px solid #c0da2f;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 3rem;
+	}
 </style>
